760f59a678a34c4cb184ab62c5382624e735b23d
Chenbo Li <lichenbo1949@gmail.com>
2013-11-12 22:05:13 +0800

    add more scala

--- scala/5.2/tictactoe.scala
+++ scala/5.2/tictactoe.scala
@@ -6,31 +6,28 @@ object TicTacToe {
 
   }
 
-
-
 }
 
 object Board {
   val board = List(List("_","_","_"),List("_","_","_"),List("_","_","_"))
+  val playerList = Tuple("O","X")
+  var player = 0
 
   def finished:Boolean = checkRows && checkColumns
-  def checkFull:Boolean = {
-    board.foreach {
-      row => row.foreach {
-        col => if (board(row)(col)=="_") false
-      }
-    }
-    true}
-  def checkRows():Boolean = for (i <- 0 to 2) {
-    checkRow(i)
-  }
+  def checkFull:Boolean = board.flatten.forall(c => c!="_")
+  def checkRows():Boolean = (0 to 2).forall(i => checkRow(i))
   def checkRow(i:Int):Boolean = board(i) == List("O","O","O") || board(i) == List("X","X","X")
-  def checkColumns():Boolean = for (i <- 0 to 2) {
-    checkCol(i)
-  }
+  def checkColumns():Boolean = (0 to 2).forall(i => checkCol(i))
   def checkCol(i:Int):Boolean = {
     val tempCol = (0 to 2).map {row => board(row)(i)}
     tempCol == List("O","O","O") || tempCol == List("X","X","X")
+  }
+  def turn = {
+    player = 1 - player
+  }
+  def put(row:Int, col:Int) = {
+    if (board(row)(col) == "-") board(row)(col) = playerList(player)
+    else println("You cannot put it here!")
   }
 }
 
